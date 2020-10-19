fe91d97c22df65516339a1ca384e95b8118662c1
Skid Vis <skid@skidvis.com>
2020-10-19 10:01:23 -0500

    Wires up to umbraco headless

--- index.js
+++ index.js
@@ -1,6 +1,35 @@
 var app = new Vue({
     el: '#app',
     data: {
-      message: 'Hello Vue!'
+      message: null,
+      imageUrl: null
+    }, 
+    created: function(){
+        console.log("mounted");
+        axios({
+            method: 'post',
+            url: 'https://graphql.umbraco.io',
+            headers: {'umb-project-alias':'vis-s-amiable-turtle', 'Content-Type': 'application/json'},
+            data:{
+                query:`{
+                    homePage(url: "/home/") {
+                      welcomeText
+                      heroImage{
+                          url
+                      }
+                    }
+                  }`            
+            }
+        })
+        .then(function (result) {
+            console.log(result.data.data.homePage.welcomeText);
+            app.message = result.data.data.homePage.welcomeText;
+            app.imageUrl = result.data.data.homePage.heroImage.url;
+        });
+    },
+    methods:{
+        doThis: function(text){
+            this.message = text;
+        }
     }
   })
